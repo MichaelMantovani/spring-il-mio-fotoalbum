11513fda210285ffa5df011d402ece18f40f38e3
MichaelMantovani <michmantovani@gmail.com>
2023-12-15 17:23:18 +0100

    add creation function for category

--- src/main/java/org/java/spring/controller/CategoryController.java
+++ src/main/java/org/java/spring/controller/CategoryController.java
@@ -9,19 +9,23 @@ import org.java.spring.db.serv.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class CategoryController {
-	
+
 	@Autowired
 	private CategoryService catServ;
-	
+
 	@Autowired
 	private PhotoService photoServ;
-	
+
 	@GetMapping("/category")
 	public String getCategoriesIndex(Model model) {
 		List<Category> categories = catServ.findAll();
@@ -36,7 +40,30 @@ public class CategoryController {
 
 		return "categDetail";
 	}
-	
+
+	@GetMapping("/category/create")
+	public String createCategory(Model model) {
+
+		model.addAttribute("category", new Category());
+
+		return "categoryForm";
+	}
+
+	@PostMapping("/category/create")
+	public String storeCategory(Model model, @Valid @ModelAttribute Category category, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+
+			model.addAttribute("cateogory", category);
+
+			return "ingredienteForm";
+		}
+
+		catServ.save(category);
+
+		return "redirect:/category";
+	}
+
 	@PostMapping("/category/{id}/delete")
 	public String deleteCategory(@PathVariable int id) {
 
