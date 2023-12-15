320dc71af622273a07953133e8ce68eb56a3127c
MichaelMantovani <michmantovani@gmail.com>
2023-12-15 11:19:46 +0100

    add show method and detail page for photo

--- src/main/java/org/java/spring/controller/PhotoController.java
+++ src/main/java/org/java/spring/controller/PhotoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PhotoController {
@@ -18,12 +19,19 @@ public class PhotoController {
 
 	
 	@GetMapping()
-	public String getPhotoIndex(Model model) {
+	public String getPhotosIndex(Model model) {
 		List<Photo> photos = photoServ.findAll();
 		model.addAttribute("photos", photos);
 		
 		return "photoIndex";
-		
 	}
+	
+	@GetMapping("/photo/{id}")
+	public String getPhotoDetail(Model model,@PathVariable int id) {
+		model.addAttribute("photo", photoServ.findById(id));
+		
+		return "photoDetail";
+	}
+	
 	
 }
