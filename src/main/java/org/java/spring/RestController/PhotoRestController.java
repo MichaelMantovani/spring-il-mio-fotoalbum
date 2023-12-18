253aa0e95995d4df1d7cb84a219941881b082a30
MichaelMantovani <michmantovani@gmail.com>
2023-12-18 11:51:55 +0100

    add photo detail function in restController

--- src/main/java/org/java/spring/RestController/PhotoRestController.java
+++ src/main/java/org/java/spring/RestController/PhotoRestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,17 @@ public class PhotoRestController {
 		model.addAttribute("photos", photos);
 
 		return new ResponseEntity<>(photos, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Photo> getPhoto(@PathVariable int id) {
+
+		Photo photo = photoServ.findById(id);
+
+		if (photo == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		return new ResponseEntity<Photo>(photo, HttpStatus.OK);
 	}
 	
 }
