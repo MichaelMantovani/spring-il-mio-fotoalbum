34c693facb595626d1112f5394b84f44213e3da1
MichaelMantovani <michmantovani@gmail.com>
2023-12-19 12:59:25 +0100

    edit photoForm

--- src/main/java/org/java/spring/controller/PhotoController.java
+++ src/main/java/org/java/spring/controller/PhotoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @Controller
